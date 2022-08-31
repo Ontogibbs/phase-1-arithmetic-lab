290d38df7d23f69eaab853f4f4411c3534e6f801
+ <+>
2022-08-31 15:51:32 -0400

    commit tree 6be4ca9aa64ab76b9a5bb8fcf0f4da021adbab6b

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+//Multiply
+let num1;
+let num2;
+let multiply;
 
-// Write your code here
+num1=1
+num2=62
+multiply = num1 * num2
+
+//Random
+let random=Math.floor(Math.random()*10+1)
+
+//Mod
+let num3 =25;
+let num4 =21;
+let mod = num3 % num4;
+
+
+//Max 
+let  max=Math.max(1,2,4,5,10,20);
